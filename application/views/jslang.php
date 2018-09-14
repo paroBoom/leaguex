1fc907a275c294c0379be2a7c1ad7d9a2302564f
ste801 <fankstribe@gmail.com>
2018-09-14 10:42:22 +0200

    New datatables language

--- application/views/jslang.php
+++ application/views/jslang.php
@@ -5,6 +5,7 @@ var fvrusername = '<?php echo lang('formvalidation_check_username');?>';
 var fvremail = '<?php echo lang('formvalidation_check_email');?>'; 
 var dtitem = '<?php echo lang('datatables_item_selected');?>';
 var dtitems = '<?php echo lang('datatables_items_selected');?>';
+var dtlang = '<?php echo lang('datatables_language');?>';
 var dtrow = '<?php echo lang('datatables_row_selected');?>';
 var dtrows = '<?php echo lang('datatables_rows_selected');?>';
 var dtdatetime = '<?php echo lang('tables_date_time');?>';
