0d1794d17ccacb0f7fc6539352161f0f6a33e466
ste801 <fankstribe@gmail.com>
2018-09-14 10:43:16 +0200

    Sidebar wrap

--- application/views/template/admin-sidebar.php
+++ application/views/template/admin-sidebar.php
@@ -1,47 +1,52 @@
 <!-- Sidebar Admin -->
 <div class="sidebar-left">
+    <div class="sidebar-wrapper">
 
-    <div class="sidebar-inner">
-        <div class="sb-club">
-            <div class="club-pic">
-                <img src="<?php echo site_url().'assets/img/club-logo/barcelona.png'; ?>" alt="logo">
-            </div> 
-            <div class="club-info">
-                <a data-toggle="collapse" href="#sbcollapse" class="collapsed">
-                    <span class="team-title">Barcellona</span>
-                </a>
-                <div class="clearfix"></div>
-                <div class="collapse in" id="sbcollapse">
-                    <ul class="nav">
-                        <li><a href="#"><span class="sb-club-menu">My Club</span></a></li>
-                        <li><a href="#"><span class="sb-club-menu">Stats</span></a></li>
-                        <li><a href="#"><span class="sb-club-menu">Stats</span></a></li>
-                    </ul>
-                </div>
-            </div>   
+        <div class="sidebar-inner">
+            <div class="sb-club">
+                <div class="club-pic">
+                    <img src="<?php echo site_url().'assets/img/club-logo/barcelona.png'; ?>" alt="logo">
+                </div> 
+                <div class="club-info">
+                    <a data-toggle="collapse" href="#sbcollapse" class="collapsed">
+                        <span class="team-title">Barcellona</span>
+                    </a>
+                    <div class="clearfix"></div>
+                    <div class="collapse in" id="sbcollapse">
+                        <ul class="nav">
+                            <li><a href="#"><span class="sb-club-menu">My Club</span></a></li>
+                            <li><a href="#"><span class="sb-club-menu">Stats</span></a></li>
+                            <li><a href="#"><span class="sb-club-menu">Stats</span></a></li>
+                        </ul>
+                    </div>
+                </div>   
+            </div>
+            <ul class="nav">
+                <li class="nav-item <?php if($this->uri->segment(1)=="home"){echo "active";}?>">
+                    <a href="<?= site_url().'home'; ?>" class="ripple"><i class="mdi mdi-keyboard-return"></i> <p><?php echo lang('adminsbar_menu_gotosite'); ?></p></a>
+                </li>
+                <li class="nav-item <?php if($this->uri->segment(2)=="dashboard"){echo "active";}?>">
+                    <a href="<?= site_url().'admin/dashboard'; ?>" class="ripple"><i class="mdi mdi-home"></i> <p><?php echo lang('adminsbar_menu_home'); ?></p></a>
+                </li>
+                <li class="nav-item <?php if($this->uri->segment(2)=="settings"){echo "active";}?>">
+                    <a data-toggle="collapse" href="#settings" class="ripple"><i class="mdi mdi-settings"></i> <p><?php echo lang('adminsbar_menu_settings'); ?></p></a>
+                    <div class="collapse" id="settings">
+                        <ul class="nav nav-collapse">
+                            <li><a href="<?= site_url().'admin/siteoptions'; ?>"><span class="submenu"><?php echo lang('adminsbar_submenu_site_options'); ?></span></a></li>
+                            <li><a href="#"><span class="submenu">Media</span></a></li>
+                            <li><a href="<?= site_url().'admin/emailoptions'; ?>"><span class="submenu">Email</span></a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item <?php if($this->uri->segment(2)=="userslist"){echo "active";}?>">
+                        <a href="<?= site_url().'admin/userslist'; ?>" class="ripple"><i class="mdi mdi-account-multiple"></i> <p><?php echo lang('adminsbar_menu_users'); ?></p></a>
+                </li>
+                <li class="nav-item <?php if($this->uri->segment(2)=="clubslist"){echo "active";}?>">
+                        <a href="<?= site_url().'admin/clubslist'; ?>" class="ripple"><i class="mdi mdi-shield"></i> <p><?php echo lang('adminsbar_menu_clubs'); ?></p></a>
+                </li>                     
+            </ul>
         </div>
-        <ul class="nav">
-            <li class="nav-item <?php if($this->uri->segment(1)=="home"){echo "active";}?>">
-                <a href="<?= site_url().'home'; ?>" class="ripple"><i class="mdi mdi-keyboard-return"></i> <p><?php echo lang('adminsbar_menu_gotosite'); ?></p></a>
-            </li>
-            <li class="nav-item <?php if($this->uri->segment(2)=="dashboard"){echo "active";}?>">
-                <a href="<?= site_url().'admin/dashboard'; ?>" class="ripple"><i class="mdi mdi-home-outline"></i> <p><?php echo lang('adminsbar_menu_home'); ?></p></a>
-            </li>
-            <li class="nav-item <?php if($this->uri->segment(2)=="settings"){echo "active";}?>">
-                <a data-toggle="collapse" href="#settings" class="ripple"><i class="mdi mdi-settings-outline"></i> <p><?php echo lang('adminsbar_menu_settings'); ?></p></a>
-                <div class="collapse" id="settings">
-                    <ul class="nav nav-collapse">
-                        <li><a href="<?= site_url().'admin/siteoptions'; ?>"><span class="submenu"><?php echo lang('adminsbar_submenu_site_options'); ?></span></a></li>
-                        <li><a href="#"><span class="submenu">Media</span></a></li>
-                        <li><a href="<?= site_url().'admin/emailoptions'; ?>"><span class="submenu">Email</span></a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item <?php if($this->uri->segment(2)=="userslist"){echo "active";}?>">
-                    <a href="<?= site_url().'admin/userslist'; ?>" class="ripple"><i class="mdi mdi-account-multiple-outline"></i> <p><?php echo lang('adminsbar_menu_users'); ?></p></a>
-            </li>                    
-        </ul>
-    </div>
 
+    </div>
 </div>
 <!-- End Sidebar Admin -->
