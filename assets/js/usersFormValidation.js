ca445520839dafa1de076c9f3aa0123f8c7c9f47
ste801 <fankstribe@gmail.com>
2018-10-13 11:27:09 +0200

    added new edit account function

--- assets/js/usersFormValidation.js
+++ assets/js/usersFormValidation.js
@@ -61,7 +61,6 @@ function signUp() {
                 }
             }
         })
-        .find('[name="birthDay"]').mask('00/00/0000').end() 
         .on('err.validator.fv', function(e, data) {
 
             if((data.field === 'username')||(data.field === 'email')||(data.field === 'password')||(data.field === 'confirmPassword')||(data.field === 'birthDay')) {
@@ -202,6 +201,111 @@ function recovery() {
             })
 
         })
+    }}
+
+/*----------------------------
+	Edit account
+------------------------------ */
+function editAccount() {
+
+    if ($('#editAccount').length && $.fn.formValidation) {
+        
+        $('.format').focus(function() {
+            $(this).attr('placeholder', '__/__/____')}).blur(function() {
+            $(this).attr('placeholder', '')
+        });
+
+        $('#editAccount').formValidation({            
+            framework: 'bootstrap4',
+            live: 'submitted',
+            locale: fvlang,
+            fields: {
+                email: {
+                    validators: {
+                        remote: {
+                            message: fvremail,
+                            url: 'admin/users/echeck_email',
+                            type: 'POST',
+                            delay: 800,
+                            data: function(validator) {
+                                return {userid: validator.getFieldElements('userid').val()};
+                            }
+                        },    
+                        notEmpty: {},
+                        regexp: {regexp:'^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$'}
+                    }
+                },
+                newpassword: {
+                    enabled: false,
+                    validators: {
+                        notEmpty: {},
+                        stringLength: {min: 4, max:20}
+                    }
+                },
+                confirmpassword: {
+                    enabled: false,
+                    validators: {            
+                        identical: {field: 'newpassword'},
+                        notEmpty: {}
+                    }
+                },
+                birthDay: {
+                    validators: {
+                        notEmpty: {},
+                        date: {format: 'DD/MM/YYYY'}               
+                    }
+                }
+            }
+        })
+        .on('keyup', '[name="newpassword"]', function(){
+
+            var isEmpty = $(this).val() === '';
+
+            $('#editAccount').formValidation('enableFieldValidators', 'newpassword', !isEmpty);
+            $('#editAccount').formValidation('enableFieldValidators', 'confirmpassword', !isEmpty);
+            
+            if($(this).val().length === 1){
+                $('#editAccount').formValidation('validateField', 'newpassword');
+                $('#editAccountForm').formValidation('validateField', 'confirmpassword');
+            }
+                
+        }) 
+        .on('err.validator.fv', function(e, data) {
+
+            if((data.field === 'email')||(data.field === 'newpassword')||(data.field === 'confirmpassword')||(data.field === 'birthDay')) {
+                data.element.data('fv.messages').find('.help-block[data-fv-for="' + data.field + '"]').hide().filter('[data-fv-validator="' + data.validator + '"]').show();
+            }
+
+        })
+        .off('success.form.fv')
+        .on('success.form.fv', function(e) {
+
+            e.preventDefault();
+
+            var $form = $(e.target);
+            
+            $.ajax({
+                url: $form.attr('action'),
+                type: 'POST',
+                data: $form.serialize(),
+                dataType: 'json',
+                success: function(response) {
+
+                    if(response.error){
+                        $.notify({message: response.message});
+                    } else {
+                        $form.formValidation('resetField', 'newpassword', true);
+                        $form.formValidation('resetField', 'confirmpassword', true);
+                        $form.formValidation('enableFieldValidators', 'newpassword', false);
+                        $form.formValidation('enableFieldValidators', 'confirmpassword', false);
+                        $.notify({message: response.message});
+                    }
+                
+                }
+            })
+
+        })
+
     }
 
 }
@@ -212,6 +316,7 @@ $(document).ready(function () {
     signUp();
     signIn();
     recovery();    
-    
+    editAccount();
+
 });
     
