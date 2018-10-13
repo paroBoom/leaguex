082fae8518ced92c4d21d2f4f7633590b1402dc2
ste801 <fankstribe@gmail.com>
2018-10-13 11:22:50 +0200

    added auth model

---
+++ application/models/Auth_model.php
@@ -0,0 +1,49 @@
+<?php 
+
+class Auth_model extends CI_Model {
+
+    function signin($email){
+
+        $this->db->select('*');
+        $this->db->from('lex_users');
+        $this->db->where('user_email', $email);
+        
+        if($query = $this->db->get()){
+            return $query->row_array();
+        } else {
+            return false; 
+        }
+        
+    }
+
+    function create_user($user_register) {
+
+        $this->db->trans_start();
+        $this->db->set('user_registered', 'NOW()', FALSE);
+        $this->db->insert('lex_users', $user_register);
+
+        $user_id = $this->db->insert_id();
+                        
+        // Init last activity
+        $this->db->set('la_user_id', $user_id, FALSE);
+        $this->db->set('la_time','NOW()', FALSE);
+        $this->db->insert('lex_last_activity');
+        $this->db->trans_complete();  
+        if ($this->db->trans_status() === FALSE){           
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    function do_forget($email) {
+        $query = $this->db->get_where('lex_users', array('user_email' => $email));
+        if ($this->db->affected_rows() >= 0) {
+            return $query->result();
+        } else {
+            return FALSE;
+        }
+    }
+
+}
+
